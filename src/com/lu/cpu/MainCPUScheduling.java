befa448688b7a295211512c04b4895ceb00eb1ef
h0103661_TC <h0103661@gmail.com>
2021-05-12 19:48:34 +0800

    start

---
+++ src/com/lu/cpu/MainCPUScheduling.java
@@ -0,0 +1,27 @@
+package com.lu.cpu;
+
+import javax.swing.JFrame;
+
+public class MainCPUScheduling extends JFrame{
+
+	private static MainCPUScheduling instance;
+	private static final long serialVersionUID = 1L;
+	private static String frameName = "109700015 …Îå¶ÈÚ CPUÅÅ³Ì";
+
+	public static void main(String[] args) {
+		instance = new MainCPUScheduling();
+        instance.init();
+	}
+	
+	public static MainCPUScheduling getInstance() {
+		return instance;
+	}
+	
+	/*
+	 * 
+	 */
+	
+	private void init() {
+		
+	}
+}
